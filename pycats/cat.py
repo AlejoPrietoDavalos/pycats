41f30cd22c4e26311f238df0a02b6aa642f9d210
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-09-15 23:41:21 -0300

    self.unit con kg en cats

--- pycats/cat.py
+++ pycats/cat.py
@@ -4,6 +4,8 @@ from datetime import date
 from pydantic import BaseModel, Field
 import pandas as pd
 
+KG = "kg"
+
 
 class Weight(BaseModel):
     weight: float
@@ -11,7 +13,8 @@ class Weight(BaseModel):
 
 class Cat(BaseModel):
     name: str
-    unit: Literal["kg"] = "kg"
+    color_hex: str = Field(description="Color de los plots asociados.")
+    unit: Literal["kg"] = KG
     weights: List[Weight] = Field(default_factory=list)
 
     def get_df_weights(self) -> pd.DataFrame:
@@ -20,6 +23,8 @@ class Cat(BaseModel):
 class Cats:
     def __init__(self, *, cats: List[Cat]):
         assert all(isinstance(c, Cat) for c in cats)
+        assert all(c.unit == KG for c in cats)  # FIXME: Solo se inserta en kg.
+        self.unit = KG
         self.names = []
         self.cats = {}
         for c in cats:
